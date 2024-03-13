5c581fcf51f507ac33982994594add6f404652ca
Norikoshmd <nrkxxshmd314@hotmail.com>
2024-03-13 21:54:43 +0900

    1st Commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,45 @@
+//good for debugging
+
+alert('This is a window alert!');
+
+console.log('This is a console test');
+
+var demo = 'this is a value';
+
+var demo2, demo3;
+    demo2 = 'apple';
+    demo3 = 'banana';
+
+    // array
+    var list = ['bread','milk','yakult'];
+
+    //array methods//
+    list.push('butter');
+
+    //unshift
+    list.unshift('cheese');
+
+    //pop
+    list.pop();
+
+    //shift
+    list.shift();
+
+
+    var person = new Object();
+
+    person.name="James";
+    person.age="43";
+    person.height = 1.76;
+    person.married = true;
+
+    person.car ={};
+
+    person.car.car1 = "Ferrari";
+    person.car.car2 = "Tesla";
+
+
+
+
+
+
