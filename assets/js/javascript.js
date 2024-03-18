46a85400b43cee8e8881be009bbb0344e0ed961c
Norikoshmd <nrkxxshmd314@hotmail.com>
2024-03-18 23:09:03 +0900

    learned Assignment Operators

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -38,6 +38,22 @@ var demo2, demo3;
     person.car.car1 = "Ferrari";
     person.car.car2 = "Tesla";
 
+    //Assignment operator
+
+    var number = 500;
+    var x = 1;
+    var y = 3;
+
+        x = y;
+
+    //compound Assignment Operator
+
+    var number1 = 10;
+    var number2 = 15;
+
+        number1 = number1 + number2;
+
+
 
 
 
